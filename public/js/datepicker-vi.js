6a4356e6c566dc603059c9444061da49955ec86a
kienbk1910 <kienbkb910@gmail.com>
2016-02-18 22:43:58 +0700

    Signed-off-by: kienbk1910 <kienbkb910@gmail.com>

--- public/js/datepicker-vi.js
+++ public/js/datepicker-vi.js
@@ -1,37 +1,26 @@
-/* Vietnamese initialisation for the jQuery UI date picker plugin. */
-/* Translated by Le Thanh Huy (lthanhhuy@cit.ctu.edu.vn). */
-( function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
+/* Vietnamese localization for the jQuery UI date picker plugin. */
+/* Written by Tien Do (tiendq@gmail.com) */
 
-		// AMD. Register as an anonymous module.
-		define( [ "../widgets/datepicker" ], factory );
-	} else {
+jQuery(function ($)
+{
+  $.datepicker.regional["vi-VN"] =
+	{
+		closeText: "Đóng",
+		prevText: "Trước",
+		nextText: "Sau",
+		currentText: "Hôm nay",
+		monthNames: ["Tháng một", "Tháng hai", "Tháng ba", "Tháng tư", "Tháng năm", "Tháng sáu", "Tháng bảy", "Tháng tám", "Tháng chín", "Tháng mười", "Tháng mười một", "Tháng mười hai"],
+		monthNamesShort: ["Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai"],
+		dayNames: ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"],
+		dayNamesShort: ["CN", "Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy"],
+		dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+		weekHeader: "Tuần",
+		dateFormat: "dd/mm/yy",
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ""
+	};
 
-		// Browser globals
-		factory( jQuery.datepicker );
-	}
-}( function( datepicker ) {
-
-datepicker.regional.vi = {
-	closeText: "Đóng",
-	prevText: "&#x3C;Trước",
-	nextText: "Tiếp&#x3E;",
-	currentText: "Hôm nay",
-	monthNames: [ "Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm", "Tháng Sáu",
-	"Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười", "Tháng Mười Một", "Tháng Mười Hai" ],
-	monthNamesShort: [ "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6",
-	"Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12" ],
-	dayNames: [ "Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy" ],
-	dayNamesShort: [ "CN", "T2", "T3", "T4", "T5", "T6", "T7" ],
-	dayNamesMin: [ "CN", "T2", "T3", "T4", "T5", "T6", "T7" ],
-	weekHeader: "Tu",
-	dateFormat: "dd/mm/yy",
-	firstDay: 0,
-	isRTL: false,
-	showMonthAfterYear: false,
-	yearSuffix: "" };
-datepicker.setDefaults( datepicker.regional.vi );
-
-return datepicker.regional.vi;
-
-} ) );
+	$.datepicker.setDefaults($.datepicker.regional["vi-VN"]);
+});
